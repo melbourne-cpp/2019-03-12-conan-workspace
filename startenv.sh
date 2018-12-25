edf57a6d2324e8c4d6da73fae4e63e0a387b1020
Lawrence Millar-Madigan <lawrencem99@gmail.com>
2018-12-26 10:22:50 +1100

    Add startenv script for Python and Conan virtual environments.

---
+++ startenv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. ~/.bashrc
+
+python3 -m venv .python-env
+
+. ./.python-env/bin/activate
+
+if [[ ! -f ./.python-env/bin/conan ]]; then
+    pip install conan
+fi
+
+export CONAN_USER_HOME="$(pwd)/.conan-user-home"
+
+conan config install ./conan-config
